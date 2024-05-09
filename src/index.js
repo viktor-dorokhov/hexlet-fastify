abfe340d80525a4474532c5a0fab2e6e68b1b4fb
Viktor Dorokhov <superdva@yandex.ru>
2024-05-09 15:35:40 +0300

    exercise 6

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import fastify from 'fastify';
 
+const state = {
+  users: [
+    {
+      id: 1,
+      name: 'user',
+    },
+  ],
+};
+
 export default () => {
   const app = fastify();
   
-  // exersice 2
+  // exercise 3
   app.get('/', (req, res) => {
     res.send('Welcome to Hexlet!');
   });
 
-  // exersice 3
+  // exercise 4
   app.get('/users', (req, res) => {
     res.send('GET /users');
   });
@@ -17,10 +26,33 @@ export default () => {
     res.send('POST /users');
   });
 
-  // exersice 4
+  // exercise 5
   app.get('/hello', (req, res) => {
     const { name = 'World' } = req.query;
     res.send(`Hello, ${name}!`);
+  });
+
+  // exercise 6
+  app.get('/courses/new', (req, res) => {
+    res.send('Course build');
+  });
+  
+  app.get('/courses/:id', (req, res) => {
+    res.send(`Course ID: ${req.params.id}`);
+  });
+
+  app.get('/courses/:courseId/lessons/:id', (req, res) => {
+    res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.id}`);
+  });
+
+  app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const user = state.users.find((user) => user.id === parseInt(id));
+    if (!user) {
+      res.code(404).send({ message: 'User not found' });
+    } else {
+      res.send(user);
+    }
   });
 
   return app;
