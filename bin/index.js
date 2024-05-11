9afe3d767606e9b9bd7dd335d377597bae6fbd57
Viktor Dorokhov <superdva@yandex.ru>
2024-05-11 15:33:54 +0300

    change host for render.com

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,6 @@ import build from '../src/index.js';
 const port = process.env.PORT || 3000;
 const app = await build();
 
-app.listen({ port }, () => {
+app.listen({ port, host: '0.0.0.0' }, () => {
   console.log(`Example app listening on port ${port}`);
 });
